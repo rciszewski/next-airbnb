df1822d4aca7736a0fea14c6576b132231faf4e9
Ron <rc72095@icloud.com>
2024-07-13 14:54:22 -0400

    add github and google buttons and already have an account with login to register modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -135,6 +135,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
