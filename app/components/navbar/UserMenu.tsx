11ce8740728fceb4aa836655fd01e4049e3d13be
Ron <rc72095@icloud.com>
2024-07-30 21:24:36 -0400

    update rentModal and add categoryINput

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -12,6 +12,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 
+
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
@@ -30,7 +31,6 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     if (!currentUser) {
       return loginModal.onOpen();
     }
-    console.log('hello')
     rentModal.onOpen();
   }, [currentUser, loginModal, rentModal]);
 
@@ -63,7 +63,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <MenuItem onClick={() => {}} label={"My favorites"} />
                 <MenuItem onClick={() => {}} label={"My reservations"} />
                 <MenuItem onClick={() => {}} label={"My properties"} />
-                <MenuItem onClick={() => {}} label={"Airbnb my home"} />
+                <MenuItem onClick={rentModal.onOpen} label={"Airbnb my home"} />
                 <hr />
                 <MenuItem
                   onClick={() => {
